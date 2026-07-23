1c4b741298eff39195f27a8f0a6996ceb9d6b358
pgreyy <preciousjioke@gmail.com>
2026-07-23 12:14:52 +0100

    remove ThruProvider from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ThruProvider } from '@thru/react-sdk'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThruProvider
-      config={{
-        iframeUrl: "https://wallet.thru.org/embedded",
-        rpcUrl: "https://grpc-web.alphanet.thruput.org",
-      }}
-    >
-      <App />
-    </ThruProvider>
+    <App />
   </React.StrictMode>,
 )
